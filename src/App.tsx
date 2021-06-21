a88332a8dffa909ff4256b08063bfd77ec36814d
christophermarek <christophermarek42@gmail.com>
2021-06-21 13:40:11 -0400

    main page finished

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,53 @@ import './App.css';
 function App() {
 
   const [currentPage, setCurrentPage] = useState<string>('landing');
-  let companyName = 'Comapny Name';
+  let companyName = 'Company Name';
+
+  let whatWeDo = {
+    title: 'What we do',
+    text: [
+      'Help you design and build a website for your buisness needs.',
+      'Your buisness needs a responsive and beautiful site.',
+      'We offer domain registration, and hosting. So that your buisness doesnt have to worry about site upkeep.'
+    ]
+  }
+
+  let howWeHelp = {
+    title: 'How we help you reach more customers',
+    text: [
+      'Bring your companies website to the front of peoples searches.',
+      'SEO to bring you to the front page of google searches (no idea how to do this btw).',
+      'We offer analytics so you can monitor user engagement on your site. (this i can do easy).'
+    ]
+  }
+
+  let helpYourBuisnessGrow = {
+    title: 'Help your buisness grow',
+    text: [
+      'A strong online prescence is important.',
+      'We will create you one',
+    ]
+  }
+
+  interface blurbText {
+    title: string
+    text: string[]
+  }
+
+  const blurb = (textValues: blurbText) => {
+    return (
+      <div className='blurb'>
+        <p className='blurbTitle'>{textValues.title}</p>
+        {textValues.text.map((element) =>
+          <p className='blurbText'>{element}</p>
+        )}
+
+        {textValues.title === 'Help your buisness grow' &&
+          <input type='button' value='Request a quote' className='btnReq' />
+        }
+      </div>
+    )
+  }
 
   return (
     <div className="App">
@@ -30,16 +76,52 @@ function App() {
               <p>{companyName}</p>
             </div>
             <div className='headerRight'>
-              <input type='button' value='Request a quote' className='btnReq'/>
-              
+              <input type='button' value='Request a quote' className='btnReq' />
             </div>
           </div>
-      </>
-    }
+
+          <div className="mainBody">
+            <div className='mainleft'>
+              {blurb(whatWeDo)}
+
+              <div className='imageBox'>
+                <img src="" />
+              </div>
+
+              {blurb(howWeHelp)}
+            </div>
+
+            <div className='mainRight'>
+              <div className='imageBox'>
+                <img src="" />
+              </div>
+              {blurb(helpYourBuisnessGrow)}
+              <div className='imageBox'>
+                <img src="" />
+              </div>
+            </div>
+          </div>
+
+          <div className='footer'>
+            <div className='nameAndDate'>
+              <p>{companyName}</p>
+              <p>2021</p>
+            </div>
+            <div className='contact'>
+              <p>Contact</p>
+              <div className='icons'>
+                <img src="" />
+                <img src="" />
+                <img src="" />
+              </div>
+            </div>
+          </div>
+        </>
+      }
 
 
-        </div>
+    </div>
   );
 }
 
-      export default App;
+export default App;
